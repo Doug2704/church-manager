e456b59ce4e5ce5533daf2ae888507785c787334
Douglas Candido <doug.candido2704@gmail.com>
2026-03-01 08:29:50 -0300

    adicionada linha vazia como teste

--- src/main/java/br/com/candido/church_manager/entity/UsuarioAutenticado.java
+++ src/main/java/br/com/candido/church_manager/entity/UsuarioAutenticado.java
@@ -24,6 +24,7 @@ public class UsuarioAutenticado implements UserDetails {
         return usuario.getPassword();
     }
 
+
     @Override
     public String getUsername() {
         return usuario.getUsername();
